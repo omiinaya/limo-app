2070b8b52bdb0e26bfc791f980a040f8dd8f149f
omiinaya <omiinaya@gmail.com>
2022-01-04 14:55:09 -0500

    cleaning up.

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Home from "./components/Home/";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CssBaseline from "@mui/material/CssBaseline";
+import "./index.css";
 
 ReactDOM.render(
   <React.Fragment>
-    <App />
+    <CssBaseline />
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} />
+      </Switch>
+    </Router>
   </React.Fragment>,
   document.getElementById('root')
 );
