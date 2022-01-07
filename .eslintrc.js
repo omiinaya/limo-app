9037c16298154817bfbedce3283d8919e4777622
Omar Minaya <omiinaya@gmail.com>
2022-01-06 19:30:17 -0500

    added mapbox map.

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = exports = {
+    "rules": {
+        "import/no-webpack-loader-syntax": "off"
+      }
+}
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Home from "./components/Home/";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
+import 'mapbox-gl/dist/mapbox-gl.css';
 import "./index.css";
 
 ReactDOM.render(
